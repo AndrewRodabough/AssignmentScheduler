59d2ae9c3436f2f6a8f873ce29849fca8e64230d
AndrewRodabough <arodabough@hotmail.com>
2024-11-08 20:30:53 -0700

    Basic Route Setup

--- src/app.jsx
+++ src/app.jsx
@@ -13,7 +13,58 @@ import './app.css';
 function App() {
 
     return (
-        
+        <BrowserRouter>
+            <div>
+                <header>
+
+                </header>
+
+                <Routes>
+                    <Route path="/home" />
+                    <Route path="/about" />
+                    
+                    <Route path="/login" />
+                    <Route path="/register" />
+                    
+                    <Route path="/main" />
+                    
+                    <Route path='*' element={<NotFound />} />
+
+
+                    <Route
+                        path='/'
+                        element={
+                            <Login
+                                userName={userName}
+                                authState={authState}
+                                onAuthChange={(userName, authState) => {
+                                    setAuthState(authState);
+                                    setUserName(userName);
+                                }}
+                            />
+                        }
+                        exact
+                    />
+
+                    {
+                        /*
+                        <Route path='/play' element={<Play userName={userName} />} />
+                        <Route path='/scores' element={<Scores />} />
+                        <Route path='/about' element={<About />} />
+                        */
+                    }
+                </Routes>
+
+                <footer>
+                    <div>
+                        <a href="https://github.com/AndrewRodabough/startup.git"> Github </a>
+                        <a href="about.html">About this project</a>
+                        Author: Andrew Rodabough
+                    </div>
+                </footer>
+
+            </div>
+        </BrowserRouter>
     )
 }
 
