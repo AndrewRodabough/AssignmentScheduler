1658d8aefb5f10ac796fb8e0966408dd8dfc04f5
AndrewRodabough <arodabough@hotmail.com>
2024-12-07 04:09:28 -0700

    added an image via "under construction" in the home page

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -3,11 +3,16 @@ import '../app.css';
 
 function Home() {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <section className="title">
-            AssignmentScheduler
-            </section>
-        </div>
+        <>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <section className="title">
+                    AssignmentScheduler
+                </section>
+                <section>
+                    <img src="../../public/construction.png" style={{ width: '100vw', height: 'auto', display: 'block', margin: 0, padding: 0 }} />
+                </section>
+            </div>
+        </>
     );
 }
 
